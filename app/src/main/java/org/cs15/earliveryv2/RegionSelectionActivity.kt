5e4e35c081c7f1db39aa89253af6500efc65592b
lukka <v20s9690@gmail.com>
2022-02-02 00:47:24 +0900

    feat : 디자인 레이아웃 코드 추가

---
+++ app/src/main/java/org/cs15/earliveryv2/RegionSelectionActivity.kt
@@ -0,0 +1,12 @@
+package org.cs15.earliveryv2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class RegionSelectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_region_selection)
+    }
+}
