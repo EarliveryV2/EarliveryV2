0cdcada9e25e285bfa80838c4c03bf93b1e4d0c9
elHongsh <elHongsh@gmail.com>
2022-02-01 20:13:25 +0900

    init: Initialize the project

---
+++ app/src/main/java/org/cs15/earliveryv2/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.cs15.earliveryv2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
